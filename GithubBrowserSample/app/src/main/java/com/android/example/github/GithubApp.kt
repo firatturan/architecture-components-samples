4c3aca2c952105fcded910f672ce627d1a8c4790
Firat Turan <firat@mobilex.com.tr>
2021-01-10 19:13:51 +0300

    Initializer added into project.

--- GithubBrowserSample/app/src/main/java/com/android/example/github/GithubApp.kt
+++ GithubBrowserSample/app/src/main/java/com/android/example/github/GithubApp.kt
@@ -18,15 +18,12 @@ package com.android.example.github
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
 class GithubApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
+
     }
 }
