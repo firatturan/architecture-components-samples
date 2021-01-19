be8db6beb3915265f5bd7c6571d2081462ac7b0c
Firat Turan <firat@mobilex.com.tr>
2021-01-19 15:12:52 +0300

    AppsFlyer integration for starter-app.

---
+++ GithubBrowserSample/app/src/main/java/com/android/example/github/initializer/AppsFlyerInitializer.kt
@@ -0,0 +1,47 @@
+package com.android.example.github.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.appsflyer.AppsFlyerConversionListener
+import com.appsflyer.AppsFlyerLib
+import timber.log.Timber
+
+class AppsFlyerInitializer : Initializer<Unit> {
+
+    companion object {
+        const val appsflyerDevKey = "###############"
+    }
+
+    override fun create(context: Context) {
+        val conversionDataListener = object : AppsFlyerConversionListener {
+            override fun onConversionDataSuccess(data: MutableMap<String, Any>?) {
+                data?.let { cvData ->
+                    cvData.map {
+                        Timber.i("Timber: conversion_attribute:  ${it.key} = ${it.value}")
+                    }
+                }
+            }
+
+            override fun onConversionDataFail(error: String?) {
+                Timber.e("Timber: error onAttributionFailure :  $error")
+            }
+
+            override fun onAppOpenAttribution(data: MutableMap<String, String>?) {
+                data?.map {
+                    Timber.d("Timber: onAppOpen_attribute: ${it.key} = ${it.value}")
+                }
+            }
+
+            override fun onAttributionFailure(error: String?) {
+                Timber.e("Timber: error onAttributionFailure :  $error")
+            }
+        }
+
+        AppsFlyerLib.getInstance().init(appsflyerDevKey, conversionDataListener, context)
+        AppsFlyerLib.getInstance().start(context)
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf(TimberInitializer::class.java)
+    }
+}
