38e7d8372f1653674ff84e0721bba73db91233bd
Firat Turan <firat@mobilex.com.tr>
2021-01-19 13:34:32 +0300

    OneSignal integration for starter-app.

---
+++ GithubBrowserSample/app/src/main/java/com/android/example/github/initializer/OneSignalInitializer.kt
@@ -0,0 +1,24 @@
+package com.android.example.github.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.onesignal.BuildConfig
+import com.onesignal.OneSignal
+
+class OneSignalInitializer : Initializer<Unit> {
+
+    override fun create(context: Context) {
+        if (BuildConfig.DEBUG) {
+            OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+        }
+        // OneSignal Initialization
+        OneSignal.initWithContext(context)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>>  = emptyList()
+
+    companion object {
+        const val ONESIGNAL_APP_ID = "########-####-####-####-############"
+    }
+}
